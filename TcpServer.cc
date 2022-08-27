66a4042f1b2bfd2cd0cbbaf60fb4833f54d89f13
jasonyichen <3100307967@qq.com>
2022-08-27 17:22:01 +0800

    user coding interface class

--- TcpServer.cc
+++ TcpServer.cc
@@ -1,1 +1,48 @@
 #include "TcpServer.h"
+#include "Logger.h"
+
+EventLoop* checkLoopNotNull(EventLoop* loop)
+{
+    if(loop==nullptr)
+    {
+        LOG_FATAL("mainLoop is null");
+    }
+    return loop;
+}
+
+TcpServer::TcpServer(EventLoop* loop,InetAddress& listenaddr,const std::string& nameArg,Option op)
+    :m_loop(checkLoopNotNull(loop)),
+    m_ipPort(listenaddr.toIpPort()),
+    m_name(nameArg),
+    m_acceptor(new Acceptor(loop,listenaddr,op = kReusePort)),
+    m_threadPool(new EventLoopThreadPool(loop,m_name)),
+    m_nextId(1)
+{
+    m_acceptor->setNewConnectionCallback(std::bind(&TcpServer::newConnection,this,
+            std::placeholders::_1,std::placeholders::_2));//new user wanna connect ,this cb will be executed
+
+}
+
+TcpServer:: ~TcpServer()
+{
+
+}
+
+void TcpServer::newConnection(int sockfd,const InetAddress& peeraddr)
+{
+
+}
+
+void TcpServer::setThreadNum(int numThreads)
+{
+    m_threadPool->setThreadPoolNumber(numThreads);
+}
+
+void TcpServer::start()
+{
+    if(m_started++==0)
+    {
+        m_threadPool->start(m_ThreadInitCallback);
+        m_loop->runInLoop(std::bind(&Acceptor::listen,m_acceptor.get()));
+    }
+}
--- TcpServer.h
+++ TcpServer.h
@@ -1,6 +1,54 @@
 #pragma once
 
+#include "EventLoop.h"
+#include "Acceptor.h"
+#include "noncopyable.h"
+#include "EventLoopThreadPool.h"
+#include "Callbacks.h"
+
+#include <functional>
+#include <string>
+#include <unordered_map>
+
 class TcpServer
 {
+public:
+    using ThreadInitCallback = std::function<void(EventLoop* loop)>;
 
+    enum Option
+    {
+        kNoReusePort,
+        kReusePort,
+    };
+
+    TcpServer(EventLoop* loop,InetAddress& listenaddr,const std::string& nameArg,Option op = kNoReusePort);
+    ~TcpServer();
+
+    void setThreadNum(int numThreads);//the number of subloop
+    void setConnectionCallback(const ConnectionCallback& cb) {m_connectioncallback = cb;}
+    void setMessageCallback(const MessageCallback& cb) {m_messageCallback =  cb;}
+
+    void start();
+
+private:
+    using ConnectionMap = std::unordered_map<std::string,TcpConnectionPtr>;
+
+    void newConnection(int sockfd,const InetAddress& peeraddr);
+    void removeConnection(const TcpConnectionPtr& conn);
+    void removeConnectionInLoop(const TcpConnectionPtr& conn);
+
+    EventLoop*                              m_loop;
+    const std::string                       m_ipPort;
+    const std::string                       m_name;
+    std::unique_ptr<Acceptor>               m_acceptor;
+    std::shared_ptr<EventLoopThreadPool>    m_threadPool;
+
+    ConnectionCallback                      m_connectioncallback;
+    MessageCallback                         m_messageCallback;
+    WriteCompleteCallback                   m_writeCallback;
+    ThreadInitCallback                      m_ThreadInitCallback;
+
+    std::atomic<int>                        m_started;
+    int                                     m_nextId;
+    ConnectionMap                           m_connections;
 };
