fa318e2228194bf19e2adf19ef0f05213da8ca2d
jasonyichen <3100307967@qq.com>
2022-08-29 15:21:29 +0800

    buffer in application layer

--- Buffer.cc
+++ Buffer.cc
@@ -1,2 +1,60 @@
 #include "Buffer.h"
 
+#include <errno.h>
+#include <sys/uio.h>
+
+void Buffer::makespace(size_t len)
+{
+    if(writeableBytes()+perPendBytes()<len+perPend)
+    {
+        m_buffer.resize(m_writeIndex+len);
+    }
+    else
+    {
+        size_t readable = readableBytes();
+        std::copy(begin()+m_readIndex,
+                  begin()+m_writeIndex,
+                  begin()+perPend);
+        m_readIndex = perPend;
+        m_writeIndex = m_readIndex+readable;
+    }
+}
+
+void Buffer::append(const char* data,size_t len) 
+{
+    ensureWriteableBytes(len);
+    std::copy(data,data+len,writeBegin());
+    m_writeIndex+=len;
+}
+
+ssize_t Buffer::readFd(int fd,int* saveError)
+{
+    char extrabuf[65536] = {0};
+    
+    struct iovec vec[2];
+    const size_t writeable = writeableBytes();
+    
+    vec[0].iov_base = begin()+m_writeIndex;
+    vec[0].iov_len = writeable;
+    vec[1].iov_base = extrabuf;
+    vec[1].iov_len = sizeof(extrabuf);
+
+    const int iovcont = (writeable<sizeof(extrabuf))?2:1;
+    const int n = readv(fd,vec,iovcont);
+
+    if(n<0)
+    {
+        *saveError = errno;
+    }
+    else if(n<=writeable)
+    {
+        m_writeIndex+=n;
+    }
+    else
+    {
+        m_writeIndex = m_buffer.size();
+        append(extrabuf,n-writeable);   
+    }
+
+    return n;
+}
--- Buffer.h
+++ Buffer.h
@@ -1,6 +1,104 @@
 #pragma once
 
+#include <vector>
+#include <sys/bitypes.h>
+#include <string>
+
 class Buffer
 {
+public:
+    static const size_t     perPend = 8;
+    static const size_t     initialSize = 1024;
 
+    explicit Buffer(size_t initialBufferSize = initialSize)
+        :m_buffer(perPend+initialSize),
+        m_readIndex(perPend),
+        m_writeIndex(perPend)
+    {}
+
+    char* begin() {return &(*m_buffer.begin());}
+
+    size_t perPendBytes() const
+    {
+        return m_readIndex;
+    }
+
+    size_t readableBytes() const
+    {
+        return m_writeIndex-m_readIndex;
+    }
+
+    size_t writeableBytes() const
+    {
+        return m_buffer.size()-m_writeIndex;
+    }
+
+    const char* begin() const 
+    {
+        return &(*m_buffer.begin());
+    }
+
+    const char* readableBegin() const 
+    {
+        return begin()+m_readIndex;
+    }
+
+    char* writeBegin()
+    {
+        return begin()+m_writeIndex;
+    }
+
+    const char* writeBegin() const
+    {
+        return begin()+m_writeIndex;
+    }
+
+    void retrieve(size_t len)
+    {
+        if(len<m_readIndex)//not finsh read all readable Bytes
+        {
+            m_readIndex+=len;
+        }
+        else
+        {
+            retrieveAll();
+        }
+    }
+
+    void retrieveAll()
+    {
+        m_readIndex = m_writeIndex = perPend;
+    }
+
+    std::string retrieveAllAsString()
+    {
+        retrieveAsString(readableBytes());
+    }
+
+    std::string retrieveAsString(size_t len)
+    {
+        std::string res(readableBegin(),len);
+        retrieve(len);
+        return res;
+    }
+
+    void ensureWriteableBytes(size_t len)
+    {
+        if(len>writeableBytes())
+        {
+            makespace(len);
+        } 
+    }
+
+    void append(const char* data,size_t len);
+
+    ssize_t readFd(int fd,int* saveError);
+
+private:
+
+    void makespace(size_t len);
+    
+    std::vector<char>       m_buffer;
+    size_t                  m_readIndex;
+    size_t                  m_writeIndex;
 };
