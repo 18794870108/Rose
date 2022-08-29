f0a1c8f7ba1fd3771a738a6b6d33b66b85a04278
jasonyichen <3100307967@qq.com>
2022-08-29 20:08:53 +0800

    abstraction of established connection

--- TcpConnection.cc
+++ TcpConnection.cc
@@ -1,2 +1,41 @@
 #include "TcpConnection.h"
+#include "Logger.h"
+
+#include <functional>
+
+static EventLoop* checkLoopNotNull(EventLoop* loop)
+{
+    if(loop==nullptr)
+    {
+        LOG_FATAL("Tcpconnection loop is null");
+    }
+    return loop;
+}
+
+TcpConnection::TcpConnection(EventLoop* loop,
+                const std::string& name,
+                int sockfd,
+                const InetAddress& localaddress,
+                const InetAddress& peeraddress):
+    m_loop(checkLoopNotNull(loop)),
+    m_name(name),
+    m_state(Connecting),
+    is_reading(true),
+    m_socket(new Socket(sockfd)),
+    m_channel(new Channel(loop,sockfd)),
+    m_localAddr(localaddress),
+    m_peerAddr(peeraddress),
+    m_highWaterMark(64*1024*1024)
+{
+    m_channel->setReadCallback(std::bind(&TcpConnection::handleRead,this,std::placeholders::_1));
+    m_channel->setWriteCallback(std::bind(&TcpConnection::handleWrite,this));
+    m_channel->setCloseCallback(std::bind(&TcpConnection::handleClose,this));
+    m_channel->setErrorCallback(std::bind(&TcpConnection::handleError,this));
+
+    m_socket->setKeepAlive(true);
+}
+
+
+TcpConnection::~TcpConnection(){}
+
 
--- TcpConnection.h
+++ TcpConnection.h
@@ -1,9 +1,81 @@
 #pragma once
 
-class TcpConnection
+#include "Channel.h"
+#include "EventLoop.h"
+#include "Socket.h"
+#include "InetAddress.h"
+#include "Callbacks.h"
+#include "Buffer.h"
+
+#include <memory>
+#include <string>
+#include <atomic>
+
+class TcpConnection:noncopyable,public std::enable_shared_from_this<TcpConnection>
 {
 public:
+    TcpConnection(EventLoop* loop,
+                const std::string& name,
+                int sockfd,
+                const InetAddress& localaddress,
+                const InetAddress& peeraddress);
+    ~TcpConnection();
+
+    EventLoop* getLoop() {return m_loop;}
+    const std::string& name() {return m_name;}
+    const InetAddress& localAddress() {return m_localAddr;}
+    const InetAddress& peerAddress()  {return m_peerAddr;}
+
+    bool connected() {return m_state==Connected;}
+
+    void send(Buffer* msg,int len);
+
+    void shutdown();
+
+    void setConnectionCallback(const ConnectionCallback& cb) {m_connectioncallback = cb;}
+    void setMessageCallback(const MessageCallback& cb) {m_messageCallback =  cb;}
+    void setWriteCompleteCallback(const WriteCompleteCallback& cb){m_writeCallback = cb;}
+    void setHighWaterMarkCallback(const HighWaterMarkCallback& cb,size_t highwater)
+    {
+        m_highwaterCallback = cb;
+        m_highWaterMark = highwater;
+    }
+    void setCloseCallback(const CloseCallback& cb){m_closeCallback = cb;}
+
+    void connectionEstablished();
+    void connectDestoryed();
 
 private:
+    enum State{Disconnected,Connecting,Connected,Disconnecting};
 
+    void handleRead(Timestamp receiveTime);
+    void handleWrite();
+    void handleClose();
+    void handleError();
+
+    void sendInLoop(const void* message,size_t len);
+
+    void shutdownInLoop();
+
+    EventLoop*                              m_loop;
+    const std::string                       m_name;
+    std::atomic<int>                        m_state;
+    bool                                    is_reading;
+
+    std::unique_ptr<Socket>                 m_socket;
+    std::unique_ptr<Channel>                m_channel;
+
+    const InetAddress                       m_localAddr;
+    const InetAddress                       m_peerAddr;
+
+    ConnectionCallback                      m_connectioncallback;
+    MessageCallback                         m_messageCallback;
+    WriteCompleteCallback                   m_writeCallback;
+    HighWaterMarkCallback                   m_highwaterCallback;
+    CloseCallback                           m_closeCallback;
+
+    size_t                                  m_highWaterMark;
+
+    Buffer                                  m_inputBuffer;
+    Buffer                                  m_outputBuffer;
 };
